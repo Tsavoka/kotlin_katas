52a5ce907b376f15de8b7f999f70c53398082720
Aenore <aenore@ya.ru>
2021-09-25 16:59:55 +0300

    6th kata

---
+++ src/main/kotlin/Acrostic.kt
@@ -0,0 +1,19 @@
+/**
+ * An acrostic is a text in which the first letter of each line spells out a word. It is also a quick and cheap way of
+ * writing a poem for somebody, as exemplified below :
+ * Write a program that reads an acrostic to identify the "hidden" word. Specifically, your program will receive a list
+ * of words (reprensenting an acrostic) and will need to return a string corresponding to the word that is spelled out
+ * by taking the first letter of each word in the acrostic.
+ */
+
+fun main() {
+    readOutTest()
+}
+
+fun readOut(acrostic: List<String>): String = acrostic.map {it[0]}.joinToString("")
+
+
+fun readOutTest() {
+    println(readOut(listOf("Jolly", "Amazing", "Courteous", "Keen")))       // 'JACK'
+    println(readOut(listOf("Marvelous", "Excellent", "Gifted")))            // 'MEG'
+}
